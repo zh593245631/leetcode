71072d58108d2a15d4480f03c0d1491cd2dd424a
张兆华 <593245631@qq.com>
2019-05-04 11:35:05 +0800

    非递归

--- 94. 二叉树的中序遍历/94. 二叉树的中序遍历/源.cpp
+++ 94. 二叉树的中序遍历/94. 二叉树的中序遍历/源.cpp
@@ -7,6 +7,7 @@
 *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 * };
 */
+//µÝ¹é
 class Solution {
 public:
 	void inorder(TreeNode* root, vector<int>& v)
@@ -22,5 +23,43 @@ public:
 		inorder(root, v);
 
 		return v;
+	}
+};
+//·ÇµÝ¹é
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root) {
+		if (!root) return{};
+
+		vector<int> ans;
+		stack<TreeNode*> s;
+		s.push(root);
+
+		while (!s.empty())
+		{
+			while (s.top())
+			{
+				s.push(s.top()->left);
+			}
+			s.pop();
+			if (!s.empty())
+			{
+				auto p = s.top();
+				ans.push_back(p->val);
+				s.pop();
+				s.push(p->right);
+			}
+		}
+
+		return ans;
 	}
 };
