d5cdb9c9a3721a464d1fb93febec93ca406c8892
张兆华 <593245631@qq.com>
2019-05-16 19:27:50 +0800

    力扣

---
+++ 268. 缺失数字/源.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+	int missingNumber(vector<int>& nums) {
+		int ret = nums.size();
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			ret ^= i;
+			ret ^= nums[i];
+		}
+		return ret;
+	}
+};
