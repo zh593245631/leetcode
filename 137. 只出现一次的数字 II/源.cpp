6fc4a4b22e2d76772d6259cb9f3831d3df06ca61
张兆华 <593245631@qq.com>
2019-06-11 09:18:10 +0800

    16 ms 9.6 MB

---
+++ 137. 只出现一次的数字 II/源.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		if (nums.size() == 1 || nums[0] != nums[1])
+			return nums[0];
+		for (int i = 1; i < nums.size() - 1; ++i)
+		{
+			if (nums[i - 1] != nums[i] && nums[i + 1] != nums[i])
+				return nums[i];
+		}
+		return nums[nums.size() - 1];
+	}
+};
--- 884. 两句话中的不常见单词/源.cpp
+++ 884. 两句话中的不常见单词/源.cpp
@@ -1,25 +1,82 @@
-class Solution {
-public:
-	vector<string> uncommonFromSentences(string A, string B) {
-		A += ' ';
-		A += B;
-		unordered_map<string, int> map;
-		vector<string> ans;
-		int start = 0;
-		int last = 0;
-		do {
-			last = A.find(' ', start);
-			string word = A.substr(start, last - start);
-			++map[word];
-			start = last + 1;
-		} while (last != -1);
+//
+//#include<vector>
+//#include<string>
+//#include<iostream>
+//#include<unordered_map>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	vector<string> uncommonFromSentences(string A, string B) {
+//		A += ' ';
+//		A += B;
+//		unordered_map<string, int> map;
+//		vector<string> ans;
+//		int start = 0;
+//		int last = 0;
+//		do {
+//			last = A.find(' ', start);
+//			string word = A.substr(start, last - start);
+//			++map[word];
+//			start = last + 1;
+//		} while (last != -1);
+//
+//		for (const auto& e : map)
+//		{
+//			if (e.second == 1) {
+//				ans.push_back(e.first);
+//			}
+//		}
+//		return ans;
+//	}
+//};
+//
+//int main()
+//{
+//	Solution sl;
+//	string A, B;
+//	getline(cin, A);
+//	getline(cin, B);
+//	sl.uncommonFromSentences(A, B);
+//	return 0;
+//}
 
-		for (const auto& e : map)
-		{
-			if (e.second == 1) {
-				ans.push_back(e.first);
-			}
-		}
-		return ans;
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+bool isS(int n)
+{
+	for (int i = 2; i <= sqrt(n); ++i)
+	{
+		if (n % i == 0)return false;
 	}
-};
+	return true;
+}
+int maxY(int n)
+{
+	for (int i = (int)sqrt(n); i > 1; --i)
+	{
+		if (n % i == 0)return i;
+	}
+	return -1;
+}
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	int cur = N;
+	int count = 0;
+
+	if (isS(N) || isS(M))cout << (-1) << endl;
+
+	while (M > cur)
+	{
+		cur += maxY(cur);
+		++count;
+	}
+
+	cout << count << endl;
+	return 0;
+}
